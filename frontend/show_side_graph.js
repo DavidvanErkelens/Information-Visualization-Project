38b360cb974d1d2afb23f466c865155f00c98bee
Fokke Dekker <fokke@dekker.email>
2018-03-18 16:50:26 +0100

    added piechart to show the percentage of attacks by each perpetrator

--- frontend/show_side_graph.js
+++ frontend/show_side_graph.js
@@ -1,12 +1,27 @@
 
 // Function to update the data in the side graphs
-function show_side_graph(country){
+function show_side_graph(countries){
+
+  // remove the old graphs
+  d3.select(".svg3").remove()
+  d3.select(".svg4").remove()
 
   // select the svg container
   var svg3 = d3.select(".svg-container2").append("svg")
   .attr("class", "svg3")
 
-  console.log(country);
+
+
+  console.log(countries);
+
+  // TODO fill with actual server data
+  all_groups = [
+    {name : "Jochem", nattack : 100},
+    {name : "Niek", nattack : 250},
+    {name : "David", nattack : 10},
+    {name : "Fokke", nattack : 200},
+    {name : "ISIS", nattack : 200},
+  ]
 
   // TODO fill with actual server data
   all_attacks = [
@@ -27,7 +42,7 @@ function show_side_graph(country){
     {date : "2017-01-15", nkill : 20033},
     {date : "2017-01-16", nkill : 2001}];
 
-    // experimental
+    /* Bar plot showing all kill for selected countries */
 
     // set the dimensions and margins of the graph
     var margin = {top: 20, right: 20, bottom: 50, left: 70},
@@ -42,10 +57,8 @@ function show_side_graph(country){
     var y = d3.scaleLinear().rangeRound([height, 0]);
     y.domain([0, d3.max(all_attacks, function(d) { return d.nkill; })]);
 
-    svgContainer = svg3
-    console.log(all_attacks);
     // create the rectangles for all selected data
-    var rects = svgContainer.selectAll("rect")
+    var rects = svg3.selectAll("rect")
     .data(all_attacks)
     .enter()
     .append("rect");
@@ -63,48 +76,26 @@ function show_side_graph(country){
     .attr("y", function(d) { return y(d.nkill); })
     .attr("class", "bar");
 
+    // Add the x Axis
+    svg3.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x)
+    .tickFormat(function (d) {return d} ));
 
-    // // add textlabel for average temp to bar
-    // text = svgContainer.selectAll("text")
-    // .data(all_attacks)
-    // .enter()
-    // .append("text");
-    //
-    // // add atributes to labels
-    // var textLabels = text
-    // .attr("x", function(d) {return x(d.month)  + (x.bandwidth()/2)})
-    // .attr("text-anchor", "middle")
-    // .attr("y", function(d) { return y(d.average_temp) + 20; })
-    // .text(function(d) {return Math.round(d.average_temp * 100)/100})
-    // .attr("font-family", "sans-serif")
-    // .attr("font-size", "20px")
-    // .attr("fill", "white");
-    //
-    // // array to translate number into month
-    // numToMonth = {"01" : "jan", "02" : "feb", "03": "mar", "04" : "apr", "05" : "may",
-    //  "06" : "jun", "07" : "jul", "08" : "aug", "09" : "sep", "10": "okt", "11" : "nov",
-    //  "12" : "dec"};
-    //
-    // // Add the x Axis
-    // svgContainer.append("g")
-    // .attr("transform", "translate(0," + height + ")")
-    // .call(d3.axisBottom(x)
-    // .tickFormat(function (d) {return numToMonth[d]} ));
-    //
-    // // text label for the x axis
-    // svgContainer.append("text")
-    // .attr("transform",
-    // "translate(" + (width/2) + " ," +
-    // (height + margin.top + 20) + ")")
-    // .style("text-anchor", "middle")
-    // .text("Month");
+    // text label for the x axis
+    svg3.append("text")
+    .attr("transform",
+    "translate(" + (width/2) + " ," +
+    (height + margin.top + 20) + ")")
+    .style("text-anchor", "middle")
+    .text("Month");
 
     // Add the y Axis
-    svgContainer.append("g")
+    svg3.append("g")
     .call(d3.axisLeft(y));
 
     // text label for the y axis
-    svgContainer.append("text")
+    svg3.append("text")
     .attr("transform", "rotate(-90)")
     .attr("y",0)
     .attr("x",0 - (height / 2))
@@ -112,22 +103,57 @@ function show_side_graph(country){
     .style("text-anchor", "middle")
     .text("Number of kills");
 
-    // // add element for current year
-    // var header_text = svgContainer.selectAll("text2")
-    // .data([year_shown])
-    // .enter()
-    // .append("text");
-    //
-    // // show the current year in the yearbox
-    // var header_text_attr = header_text
-    // .attr("x", width/2)
-    // .text(function(d) {return d})
-    // .attr("font-weight", "bold")
-    // .attr("font-size", "20px");
+    /* End barplot for selectede countries*/
+
+    /*pie chart for showing perpetrators percentages*/
+
+    var width = 250,
+    height = 250,
+    radius = Math.min(width, height) / 2;
+
+    var color = d3.scaleOrdinal()
+    .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+
+    var arc = d3.arc()
+    .outerRadius(radius - 10)
+    .innerRadius(radius - 70);
+
+    var pie = d3.pie()
+    .sort(null)
+    .value(function (d) {
+      return d.nattack;
+    });
 
 
-    // experimental
+    var svg4 = d3.select(".svg-container2").append("svg")
+    .attr("class", "svg4")
+    .attr("width", width)
+    .attr("height", height)
+    .append("g")
+    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
+    var g = svg4.selectAll(".arc")
+    .data(pie(all_groups))
+    .enter().append("g")
+    .attr("class", "arc");
+
+    g.append("path")
+    .attr("d", arc)
+    .style("fill", function (d) {
+      return color(d.name);
+    });
+
+    g.append("text")
+    .attr("transform", function (d) {
+      return "translate(" + arc.centroid(d) + ")";
+    })
+    .attr("dy", ".35em")
+    .style("text-anchor", "middle")
+    .text(function (d) {
+      return String(d.name) + " (" + String(d.nattack) + ")";
+    });
+
+    /*end pie chart showing perpetrators percentages*/
 
 
   }

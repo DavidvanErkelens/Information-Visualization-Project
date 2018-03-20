b48eafd478b8fdcb59dbd04eca6db64f19bc7c31
ndevisscher <devisscher.niek@gmail.com>
2018-03-20 14:53:34 +0100

    legenda for timegraph

--- frontend/timegraph.js
+++ frontend/timegraph.js
@@ -64,9 +64,41 @@ function drawTime(containerSelector, data) {
 
 	// d3.selectAll(".svgTime").remove();
 
+	var legendRectSize = 18;                                
+    var legendSpacing = 4;
+    var color = d3.scaleOrdinal(d3.schemeCategory20b);
+
+	var legend = svg.selectAll('.legend')                     
+          .data([timeData.label])                                   
+          .enter()                                               
+          .append('g')                                            
+          .attr('class', 'legend')                                
+          .attr('transform', function(d, i) {                    
+            var height = legendRectSize + legendSpacing;          
+            var offset =  height * 2 / 2;     
+            var horz = 2 * legendRectSize;                       
+            var vert = i * height - offset;                    
+            return 'translate(' + horz + ',' + vert + ')';        
+          });                                                     
+
+        legend.append('rect')                                     
+          .attr('width', legendRectSize)                          
+          .attr('height', legendRectSize)                         
+          .style('fill', color)                                   
+          .style('stroke', color);                                
+          
+        legend.append('text')                                     
+          .attr('x', legendRectSize + legendSpacing)              
+          .attr('y', legendRectSize - legendSpacing)              
+          .text(function(d) { return d; });                       
+
 };
 
 
+var timeData = [
+	{ label: 'Total attacks', count: 10 }, 
+    { label: 'Attacks for filter', count: 20 }
+];
 
 var lineData = [{
   year: 2001,

347fe091b10b0d473aaecde6911f14398b0c01c5
ndevisscher <devisscher.niek@gmail.com>
2018-03-21 17:04:29 +0100

    update

--- frontend/timegraph.js
+++ frontend/timegraph.js
@@ -83,9 +83,9 @@ function drawTime(containerSelector, data) {
 };
 
 // Timegraph request dictionary with static start and end
-var timeDict = {0:{1:true, 2:false, 3:true, 4:false, 5:false, 6:false, 7:false, 8: false, 9:false},
+var timeDict = {0:{1:false, 2:false, 3:true, 4:false, 5:false, 6:false, 7:false, 8: false, 9:false},
 				  1:{1:true, 2:false, 3:false, 4:false, 5:false, 6:false, 7:false, 8: false, 9:false, 10:false, 11:false, 12:false, 13:false, 14:false, 15:false, 16:false, 17: false, 18:false, 19: false, 20: false, 21: false, 22: false},
-				  2:{1:true, 2:false, 3:false, 4:false, 5:false, 6:false, 7:false, 8: false, 9:false, 10:false, 11:false, 12:false, 13:false},
+				  2:{1:false, 2:false, 3:false, 4:false, 5:false, 6:true, 7:false, 8: false, 9:false, 10:false, 11:false, 12:false, 13:false},
 				  3:{1:false, 2:true, 3:false, 4:false, 5:false, 6:false, 7:false, 8: false, 9:false, 10:false, 11:false, 12:false, 13:false, 14: false, 15: false},
 				  4:{1:true, 2:false, 3:false, 4:false, 5:false},
 					"time" : {"start" : 1970, "end" : 2015},
